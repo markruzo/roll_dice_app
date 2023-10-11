4fe877023df585d2d3808f1a5b482b8d18364398
Mario Krutzikowsky <mario.krutzikowsky@gmail.com>
2023-10-11 07:33:20 +0200

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+import 'package:basics/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Material();
-  }
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 33, 5, 109),
+          Color.fromARGB(255, 68, 21, 149),
+        ),
+      ),
+    ),
+  );
 }
